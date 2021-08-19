863d46b615c3fd8e9feeb3e2fd131a28a708c3a9
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-19 09:52:33 -0500

    Registered Amdin site and created serializers.py

--- youtube_clone_project/youtube_clone/models.py
+++ youtube_clone_project/youtube_clone/models.py
@@ -2,10 +2,9 @@ from django.core.exceptions import DisallowedRedirect
 from django.db import models
 
 # Create your models here.
-class Comments(models.Model):
+class Comment(models.Model):
     video_id = models.CharField(max_length=100)
     comment = models.CharField(max_length=500)
     replies = models.CharField(max_length=500)
     likes = models.IntegerField(max_length=None)
     dislikes = models.IntegerField(max_length=None)
-    
